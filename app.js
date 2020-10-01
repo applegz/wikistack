28a8cac83d2dc022e029331a2a1624d5b6b138e6
Adria Orenstein <adria.orenstein@gmail.com>
2020-10-01 16:07:52 -0400

    added models and layout

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const layout = require('./views/layout');
+//const { addPage, editPage, main, userList, userPages, wikiPage } = require('./views/index');
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 
-app.get('/', (req, res, next) => {
-  req.send('hello world');
+app.get('/layout', (req, res, next) => {
+  res.send(layout(''));
 });
 
-const PORT = 1338;
+app.get('/', (req, res, next) => {
+  res.send('hello world');
+});
+
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
